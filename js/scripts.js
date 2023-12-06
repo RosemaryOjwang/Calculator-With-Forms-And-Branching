dd5b64bd83d9015788880ed6392d7053939f5726
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-12-06 11:14:54 +0300

    implement branchind and print results

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,25 @@ function divide(num1, num2) {
 //User Interface Logic
 function handleCalculation(event) {
     event.preventDefault();
+    //get and process form values
     const number1 = parseInt(document.querySelector("input#input1").value);
     const number2 = parseInt(document.querySelector("input#input2").value);
     const operator = document.querySelector("input[name='operator']:checked").value;
-    //code to get and process form value will go here!
+    
+    let result;
+    if (operator === "add") {
+        result = add(number1, number2);
+    }   else if (operator === "subtract")   {
+        result = subtract(number1, number2);
+    }   else if (operator === "multiply")   {
+        result = multiply(number1, number2);
+    }   else if (operator === "divide") {
+        result = divide(number1, number2);
+    }
+
+    document.getElementById("output").innerText = result;
+
+    
 }
 
 window.addEventListener("load", function()  {
