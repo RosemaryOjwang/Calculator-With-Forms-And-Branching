70806d930206045d944bd3971fd0ff440c960f18
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-12-06 10:33:03 +0300

    addEventListeners to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 //Business Logic
 function add(num1, num2)    {
-    return num1 + num2
+    return num1 + num2;
 }
 
 function subtract(num1, num2)   {
-    return num1 - num2
+    return num1 - num2;
 }
 
 function multiply(num1, num2)   {
-    return num1 * num2
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2
+    return num1 / num2;
 }     
 
 //User Interface Logic
+function handleCalculation(event) {
+    event.preventDefault();
+    //code to get and process form value will go here!
+}
+
+window.addEventListener("load", function()  {
+    const form = document.getElementById("calculator");
+    form.addEventListener("submit", handleCalculation);
+});
 
